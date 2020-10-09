e221595eb87fc4f42e5f35bb4fed8f33043283e5
macbookpro <bratukhinmaks@gmail.com>
2020-10-09 18:47:11 +0200

    wloska fixed

--- js/custom.js
+++ js/custom.js
@@ -5,5 +5,7 @@ function myFunction() {
 }
 
 function showPage() {
-  document.getElementById("loader-container").style.display = "none";
+  console.log("Hello")
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
 }
